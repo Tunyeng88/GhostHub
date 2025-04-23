a892bf037f90e7d35138fb86aca24852e662a7ac
BleedingXiko <66162192+BleedingXiko@users.noreply.github.com>
2025-04-22 21:41:33 -0700

    UX - start indexing on add

--- app/routes/api_routes.py
+++ app/routes/api_routes.py
@@ -39,6 +39,10 @@ def add_category():
 
     try:
         new_category, error = CategoryService.add_category(name, path)
+        # Start indexing category
+        if new_category and 'id' in new_category:
+            MediaService.start_async_indexing(new_category['id'], new_category['path'], new_category['name'])
+
         if error:
             # Determine appropriate status code based on error
             status_code = 400 if "exists" in error or "not a directory" in error else 500
